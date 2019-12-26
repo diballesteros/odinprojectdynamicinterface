b877c810885fe7b846194fcd1f0d3c15062b80de
Diego <diballesterosc@gmail.com>
2019-12-26 15:21:15 -0500

    Styling button, display function

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 /*Position, scale, rotation, opacity */
 //https://www.youtube.com/watch?v=AIdslaUj9wg
+
+(() => {
+    const expandMenu = () => {
+        document.getElementById('menulist').classList.toggle('active');
+    };
+
+    document.getElementById('dropdown').addEventListener('click', expandMenu);
+})()
