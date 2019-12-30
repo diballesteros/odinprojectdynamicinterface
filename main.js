793b4473756984804ac50d49ce20920c2e318c2a
Diego Ballesteros <diballesterosc@gmail.com>
2019-12-29 20:56:36 -0500

    image slider skeleton

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 /*Position, scale, rotation, opacity */
 //https://www.youtube.com/watch?v=AIdslaUj9wg
+const expandMenu = () => {
+    document.getElementById('menulist').classList.toggle('active');
+};
 
-(() => {
-    const expandMenu = () => {
-        document.getElementById('menulist').classList.toggle('active');
-    };
+document.getElementById('dropdown').addEventListener('click', expandMenu);
 
-    document.getElementById('dropdown').addEventListener('click', expandMenu);
-})()
+const ImageSlider = function ImageSlider() {
+    const images = [];
+    let currentPosition = 0;
+
+    const transitionForwards = () => {
+
+    }
+
+    const transitionBackwards = () => {
+
+    }
+
+    const changeImage = () => {
+        
+    }
+};
+
